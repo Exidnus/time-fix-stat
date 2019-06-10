156b7d6865952d9256665b634df1ddfb9e95b168
varygin <varygin@protei.ru>
2019-06-10 17:22:04 +0300

    add routing for single http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate rouille;
+
+use rouille::Request;
+use rouille::Response;
+
 fn main() {
-    println!("Hello, world!");
+    rouille::start_server("127.0.0.1:4545", move |request| {
+        handle_request(&request)
+    })
+}
+
+fn handle_request(request: &Request) -> Response {
+    rouille::router!(request,
+        (GET) (/v1/get_all_stat_for_month/year/{ year: i32 }/month/{ month: i32 }) => {
+            Response::text("Json should be here, but not now.")
+        },
+        _ => Response::empty_404()
+    )
 }
