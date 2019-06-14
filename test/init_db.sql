b8aecc3b22f8c840f443b243f84d2d078265d0b6
varygin <varygin@protei.ru>
2019-06-14 17:51:30 +0300

    add Dockerfile for tests db (postgres)

---
+++ test/init_db.sql
@@ -0,0 +1,15 @@
+CREATE USER time_fix_stat_user WITH password 'time_fix_stat';
+ALTER USER time_fix_stat_user WITH superuser;
+
+CREATE DATABASE time_fix_stat
+    WITH OWNER = time_fix_stat_user
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
+
+\connect time_fix_stat;
+
+CREATE SCHEMA IF NOT exists time_fix_stat
+    AUTHORIZATION time_fix_stat_user;
